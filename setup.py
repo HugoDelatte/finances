5deb473bd636b776ea79841477bf2d275da9ffcb
HugoDelatte <delatte.hugo@gmail.com>
2019-09-23 09:04:55 +0200

    Version 1.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='finances',
-    version='1.4.0',
+    version='1.5.0',
     author='Hugo Delatte',
     author_email='delatte.hugo@gmail.com',
     description='Analyse your HSBC statements ',
